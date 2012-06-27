769d658aabe0dba416b1ad694bcc81ef6bff5091
Wade Berrier <wberrier@gmail.com>
2012-06-27 13:41:31 -0600

    be able to return partial results
    done by requiring search to have a pre-allocated connection

--- mutt-ldap.py
+++ mutt-ldap.py
@@ -87,29 +87,21 @@ def connect():
             ldap.AUTH_SIMPLE)
     return connection
 
-def search(query, connection=None):
-    local_connection = False
-    try:
-        if not connection:
-            local_connection = True
-            connection = connect()
-        post = ''
-        if query:
-            post = '*'
-        filterstr = '(|%s)' % (
-            u' '.join([u'(%s=*%s%s)' % (field, query, post)
-                       for field in CONFIG.get('query', 'search_fields').split()]))
-        filter = CONFIG.get('query', 'filter')
-        if filter:
-            filterstr = u'(&(%s)%s)' % (filter, filterstr)
-        r = connection.search_s(
-            CONFIG.get('connection', 'basedn'),
-            ldap.SCOPE_SUBTREE,
-            filterstr.encode('utf-8'))
-    finally:
-        if local_connection and connection:
-            connection.unbind()
-    return r
+def search(query, connection):
+      post = ''
+      if query:
+          post = '*'
+      filterstr = '(|%s)' % (
+          u' '.join([u'(%s=*%s%s)' % (field, query, post)
+                     for field in CONFIG.get('query', 'search_fields').split()]))
+      filter = CONFIG.get('query', 'filter')
+      if filter:
+          filterstr = u'(&(%s)%s)' % (filter, filterstr)
+      msg_id = connection.search(
+          CONFIG.get('connection', 'basedn'),
+          ldap.SCOPE_SUBTREE,
+          filterstr.encode('utf-8'))
+      return msg_id
 
 def format_columns(address, data):
     yield address
@@ -170,9 +162,21 @@ if __name__ == '__main__':
     (cache_hit, addresses) = cache_lookup(query)
 
     if not cache_hit:
-        entries = search(query)
-        addresses = list(itertools.chain(
-                *[format_entry(e) for e in sorted(entries)]))
+        connection = connect()
+        msg_id = search(query, connection)
+
+        # wacky, but allows partial results
+        while True:
+            try:
+                res_type, res_data = connection.result(msg_id, 0)
+            except ldap.ADMINLIMIT_EXCEEDED:
+                #print "Partial results"
+                break
+            # last result will have this set
+            if res_type == ldap.RES_SEARCH_RESULT:
+                break
+
+            addresses += [entry for entry in format_entry(res_data[-1])]
 
         # Cache results for next lookup
         cache_persist(query, addresses)
